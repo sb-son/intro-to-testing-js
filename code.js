c8d5995e7a56de6110ceee1ccf00de7945ae26d2
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-10-13 11:16:59 -0500

    add the first test and soluton for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,16 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function subtract(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x-y;
+    } else {
+        return false;
+    }
+}
+//Exercise 1
+function sayHello(){
+    return "Hello";
+}
+//Exercise 2
--- tests.js
+++ tests.js
@@ -13,3 +13,44 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe("subtract", function(){
+   it("should be a defined function", function(){
+       expect(typeof subtract).toBe('function');
+   });
+   it("should return a number when passed two number arguments", function(){
+      expect(typeof subtract(3,1)).toBe("number");
+   });
+    it("should return 75 when passed the arguments of 100 and 25", function() {
+        expect(subtract(100,25)).toBe(75);
+    });
+   it("should return false when passed any argument that is not a number", function(){
+       expect(subtract()).toBe(false);
+       expect(subtract(2)).toBe(false);
+       expect(subtract("Banana", 6)).toBe(false);
+   });
+   it("should return 34 when passed the arguments of 64 and 30", function() {
+      expect(subtract(64,30)).toBe(34);
+   });
+    it("should return -7 when passed the arguments of 0 and -7", function() {
+        expect(subtract(0,7)).toBe(-7);
+    });
+});
+
+//Exercise 1
+describe("sayHello", function (){
+    it('should be a defined function', function (){
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function (){
+       expect(typeof sayHello()).toBe('string');
+    });
+    it('should say "Hello" to any string we pass to it', function (){
+        expect(sayHello()).toBe('Hello');
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+//Exercise 2
